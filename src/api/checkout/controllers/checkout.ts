b6538efb6011b76b143634718b82663a6f1f964e
Rémi PONCHE <ponche62880@gmail.com>
2024-05-08 23:59:21 +0200

    👔 Ajout le controlleur de demande de page Stripe
    le controlleur en question recupere la commande Stripe afin de crée la session de la page checkout de Stripe. et redonne URL pour la redirection au client.

--- src/api/checkout/controllers/checkout.ts
+++ src/api/checkout/controllers/checkout.ts
@@ -2,53 +2,75 @@
  * A set of functions called "actions" for `checkout`
  */
 
-import { Context , Next } from "koa";
+import { Context, Next } from "koa";
 import Stripe from "stripe";
-import { factories } from "@strapi/strapi";
-import { validate } from "@strapi/utils"
+import Joi from "joi";
 
 const YOUR_DOMAIN = process.env.URL_FRONT;
-const TOKEN_STRIPE = process.env.TOKEN_STRIPE; 
+const TOKEN_STRIPE = process.env.TOKEN_STRIPE;
 
 const stripe = new Stripe(TOKEN_STRIPE);
 
+// schema pour le controlleur
+const schema = Joi.array().items(
+  Joi.object({
+    offre: Joi.number().required(),
+    quantity: Joi.number().required(),
+  })
+);
 
 export default {
-  sendCommand: async (ctx: Context & {request: { body: any}}, next: Next) => {
-    console.log(ctx.request.body);
-    console.log("Custom controller");
+  sendCommand: async (
+    ctx: Context & {
+      request: { body: Array<{ offre: number; quantity: number }> };
+    },
+    next: Next
+  ) => {
+    try {
+      // vérification des data
+      Joi.assert(ctx.request.body, schema, "format Invalide");
 
-    // const contentType = strapi.contentType("api:checkout.checkout"); 
-
-    //await 
-
-    // transformation des data pour stripe 
-
-    //const line_items = 
-
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+      let promiseResult = ctx.request.body.map(async (item) => {
+        const entityOffre = await strapi.entityService.findOne(
+          "api::offre.offre",
+          item.offre
+        );
+        const lineItemStrip = {
           price_data: {
             currency: "EUR",
             product_data: {
-              name: "Commande JO",
+              name: entityOffre.name,
             },
-            unit_amount: 3000,
+            unit_amount: entityOffre.price,
           },
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      success_url: `${YOUR_DOMAIN}?success=true`,
-      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-    });
+          quantity: item.quantity,
+        };
 
-    console.log(session.url);
-    //ctx.response.redirect(session.url); // Problème cors avec request sur OPTION de firefox
-    ctx.body = session.url; 
-    
-    next();
+        return lineItemStrip;
+      });
+
+      const resultListItemStripe = await Promise.all(promiseResult);
+
+      console.log(resultListItemStripe);
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: resultListItemStripe,
+        mode: "payment",
+        success_url: `${YOUR_DOMAIN}?success=true`,
+        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+      });
+
+      console.log(session.url);
+      //ctx.response.redirect(session.url); // Problème cors avec request sur OPTION de firefox
+      ctx.body = session.url;
+
+      next();
+    } catch (err) {
+      ctx.response.status = 400; // Bad Request
+      console.log("---début error---");
+      console.log(err);
+      console.log("---fin error---");
+      ctx.response.body = err;
+    }
   },
 };
